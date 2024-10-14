ecfd6ac2f88cc533efcff9338c667f316a3364a9
Vinitkumar Gaikwad <vinit.kumar@currencysolutions.com>
2024-10-14 17:22:26 +0100

    storage implemented using json file as persistent storage

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "log"
 
 func main() {
 	data := new(TodoData)
+	storage := NewStorage("todo.json")
+
 	err := Execute(data)
 	if err != nil {
 		log.Fatal(err)
---
+++ storage.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Storage struct {
+	fileName string
+}
+
+func NewStorage(fileName string) *Storage {
+	return &Storage{fileName: fileName}
+}
+
+func (s *Storage) LoadData(data *TodoData) error {
+	byt, err := os.ReadFile(s.fileName)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if len(byt) > 0 {
+		err = json.Unmarshal(byt, &data.Todos)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *Storage) SaveData(data *TodoData) error {
+	byt, err := json.MarshalIndent(data.Todos, "", "	")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = os.WriteFile(s.fileName, byt, 0644)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
